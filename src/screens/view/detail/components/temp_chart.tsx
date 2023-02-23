d8721f5b737d1010d893e4bb9fe04d37c5fdad88
Vis Cognitionis <omeralemdar94@gmail.com>
2023-02-23 04:43:19 +0300

    Update temp_chart.tsx

--- src/screens/view/detail/components/temp_chart.tsx
+++ src/screens/view/detail/components/temp_chart.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { View } from "react-native";
 import { LineChart } from "react-native-chart-kit";
 import { AbstractChartConfig } from "react-native-chart-kit/dist/AbstractChart";
+
 import { useTheme } from "src/core/init/themes/theme_context";
 import { windowWidth } from "../../common/constants/constants";
-
+import { WeatherDatas } from "../../home/interfaces/interface_home";
 import {
   groupWeatherDataByDate,
   today,
   tomorrow,
 } from "../../home/components/weather_all";
-import { WeatherDatas } from "../../home/interfaces/interface_home";
 
 const TemperatureChart = ({ weatherDatas }: { weatherDatas: WeatherDatas }) => {
   const { theme } = useTheme();
@@ -40,36 +40,79 @@ const TemperatureChart = ({ weatherDatas }: { weatherDatas: WeatherDatas }) => {
     ? [...todaysTempArr, ...tomorrowsTempArr]
     : tomorrowsTempArr;
 
+  // 24 saatlik tam döngü için
+
+  // const todaysWeather = groupWeatherDataByDate(weatherDatas)[today];
+  // const tomorrowsWeather = groupWeatherDataByDate(weatherDatas)[tomorrow];
+
+  // const todaysHourArr = todaysWeather?.map((weather) => {
+  //   return weather.dt_txt.split(" ")[1].slice(0, 2);
+  // });
+  // const todaysTempArr = todaysWeather?.map((weather) => {
+  //   return weather.main.temp;
+  // });
+
+  // const tomorrowsHourArr = tomorrowsWeather?.map((weather) => {
+  //   return weather.dt_txt.split(" ")[1].slice(0, 2);
+  // });
+  // const tomorrowsTempArr = tomorrowsWeather?.map((weather) => {
+  //   return weather.main.temp;
+  // });
+
+  // const now = new Date();
+  // const combinedHourArr =
+  //   todaysWeather && tomorrowsWeather
+  //     ? [
+  //         ...todaysHourArr,
+  //         ...tomorrowsHourArr.filter(
+  //           (hour, index) =>
+  //             parseInt(hour) < now.getHours() && index < todaysHourArr.length
+  //         ),
+  //       ]
+  //     : todaysWeather
+  //     ? todaysHourArr
+  //     : tomorrowsHourArr;
+
+  // const combinedTempArr =
+  //   todaysWeather && tomorrowsWeather
+  //     ? [
+  //         ...todaysTempArr,
+  //         ...tomorrowsTempArr.filter(
+  //           (hour, index) =>
+  //             parseInt(hour.toString()) < now.getHours() &&
+  //             index < todaysTempArr.length
+  //         ),
+  //       ]
+  //     : todaysWeather
+  //     ? todaysTempArr
+  //     : tomorrowsTempArr;
+
   const chartConfig: AbstractChartConfig = {
     backgroundGradientFrom: theme.palette.secondary?.light,
     backgroundGradientTo: theme.palette.secondary?.light,
     color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
     decimalPlaces: 0,
     propsForDots: {
-      r: "3",
+      r: "2.5",
     },
     propsForHorizontalLabels: {
-      fontSize: 10,
-      fontWeight: "bold",
+      fontSize: 12,
+      fontFamily: "Poppins-Regular",
       fill: theme.palette.primary.dark,
     },
 
     propsForVerticalLabels: {
-      fontSize: 10,
-      fontWeight: "bold",
+      fontFamily: "Poppins-Regular",
+      fontSize: 12,
       fill: theme.palette.primary.dark,
     },
-    formatYLabel: (yLabel) => `${yLabel}°`,
-    // propsForBackgroundLines: {
-    //   fill: "red",
-    // },
   };
 
   return (
     <LineChart
       // withDots={false}
       // withOuterLines={false}
-      withShadow={false}
+      // withShadow={false}
       // withInnerLines={false}
       withVerticalLines={false}
       data={{
@@ -84,16 +127,14 @@ const TemperatureChart = ({ weatherDatas }: { weatherDatas: WeatherDatas }) => {
       }}
       width={windowWidth - 60}
       height={200}
-      style={{ marginLeft: 30, borderRadius: 16 }}
-      chartConfig={{
-        ...chartConfig,
-        style: {
-          borderStyle: "solid",
-          borderWidth: 1,
-          borderColor: "#ff0000",
-        },
+      style={{
+        borderRadius: 16,
+        alignSelf: "center",
       }}
+      chartConfig={chartConfig}
       yAxisSuffix="°"
+      yLabelsOffset={26}
+      segments={5}
     />
   );
 };
