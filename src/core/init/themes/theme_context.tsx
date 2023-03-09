663d1a17237917064d57015d70063d305373bb92
Vis Cognitionis <omeralemdar94@gmail.com>
2023-03-09 22:53:28 +0300

    Add AsyncStorage in theme

--- src/core/init/themes/theme_context.tsx
+++ src/core/init/themes/theme_context.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
 import { Appearance } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import ThemeProps from "./interface/interfaces";
 import darkTheme from "./styles/dark";
 import lightTheme from "./styles/light";
@@ -15,12 +17,39 @@ const ThemeContext = createContext<ThemeContext>({
 });
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<ThemeProps>(
-    Appearance.getColorScheme() === "dark" ? darkTheme : lightTheme
-  );
+  const [theme, setTheme] = useState<ThemeProps>(lightTheme);
+
+  useEffect(() => {
+    (async () => {
+      const themeFromStorage = await AsyncStorage.getItem("userTheme");
+
+      if (themeFromStorage !== null && themeFromStorage === "light") {
+        setTheme(lightTheme);
+      } else if (themeFromStorage === "dark") {
+        setTheme(darkTheme);
+      } else {
+        const colorScheme = Appearance.getColorScheme();
+        if (colorScheme === "dark") {
+          setTheme(darkTheme);
+        } else {
+          setTheme(lightTheme);
+        }
+      }
+    })();
+  }, []);
+
+  const handleSetTheme = async (theme: ThemeProps) => {
+    try {
+      setTheme(theme);
+      const setUserTheme = theme === lightTheme ? "light" : "dark";
+      await AsyncStorage.setItem("userTheme", setUserTheme);
+    } catch (e) {
+      console.log("Error setting theme to storage:", e);
+    }
+  };
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme, setTheme: handleSetTheme }}>
       {children}
     </ThemeContext.Provider>
   );
