9e00426a3d936488322f7180d60bc42dbb4bcba9
Vis Cognitionis <omeralemdar94@gmail.com>
2023-03-11 17:06:32 +0300

    Update statusbar_settings.tsx

--- src/screens/view/settings/components/statusbar_settings.tsx
+++ src/screens/view/settings/components/statusbar_settings.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { observer } from "mobx-react";
-import { View } from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { observer } from "mobx-react";
 
 import mainStore from "src/screens/view-model/main_store";
 import { useTranslate } from "src/core/init/lang/custom-hook/useTranslate";
@@ -12,16 +11,14 @@ const StatusBarSettings = ({ content }: { content: string }) => {
   const { t } = useTranslate();
 
   return content === t("settings.general.statusBar") ? (
-    <View>
-      <BouncyCheckbox
-        isChecked={!mainStore.hideStatusBar}
-        fillColor={theme.palette.success?.main}
-        style={{ width: 25 }}
-        onPress={() => {
-          mainStore.setHideStatusBar(!mainStore.hideStatusBar);
-        }}
-      />
-    </View>
+    <BouncyCheckbox
+      isChecked={!mainStore.hideStatusBar}
+      fillColor={theme.palette.success?.main}
+      style={{ width: 25 }}
+      onPress={() => {
+        mainStore.setHideStatusBar(!mainStore.hideStatusBar);
+      }}
+    />
   ) : null;
 };
 
